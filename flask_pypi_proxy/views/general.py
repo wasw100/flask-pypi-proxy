57b04ca2e44d66e171642ba20b7a4e4b1aea91cd
wasw100 <wasw100@gmail.com>
2016-03-06 15:02:40 +0800

    simple page

--- flask_pypi_proxy/views/general.py
+++ flask_pypi_proxy/views/general.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+import os
 from urlparse import urljoin
 from contextlib import closing
-from flask import Blueprint, request, Response, stream_with_context
+from flask import (
+    Blueprint, render_template, request, Response, stream_with_context)
 import requests
 
 from flask_pypi_proxy.ext import pypi
@@ -16,12 +18,24 @@ def generate():
             yield data
 
 
+@mod.route('/simple/')
+def simple():
+    packages = []
+    for filename in os.listdir(pypi.base_folder_path):
+        if filename == '.gitignore':
+            continue
+        packages.append(filename)
+
+    packages.sort()
+    return render_template('simple.html', packages=packages)
+
+
 @mod.route('/simple/<package_name>/')
 def package_name(package_name):
     return Response(stream_with_context(generate()))
 
 
 @mod.route('/packages/<package_type>/<letter>/<package_name>/<package_file>',
-                   methods=['GET'])
+           methods=['GET'])
 def packages(package_type, letter, package_name, package_file):
     return Response(stream_with_context(generate()))
